959bfb1aacdd5e26d23f676247c9322f4a75597e
Oscar Guerrero <oscarguerrero242@gmail.com>
2022-09-01 13:36:43 -0500

    improvements

--- Ejercicio 9/index.js
+++ Ejercicio 9/index.js
@@ -3,17 +3,34 @@ function ejercicio () {
 }
 console.log(ejercicio())
 
-async function funcionAsincrona () {
+const funcionAsincrona = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('Soy una promesa')
   }, 5000)
+})
+
+funcionAsincrona
+  .then(() => console.log('Se ejecuto asincrono'))
+  .catch(() => console.log('ERROR'))
+  .finally(() => 'Ejecutado')
+
+function * generaPares () {
+  let id = 0
+  while (true) {
+    id += 2
+    yield id
+    if (id > 10) return
+  }
 }
+const pares = generaPares()
 
-funcionAsincrona()
-
-function indicesPares (current) {
-  return current % 2 === 0 ? current + 2 : current + 1
-}
-
-console.log(indicesPares(2))
-console.log(indicesPares(1))
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
+console.log(pares.next().value)
