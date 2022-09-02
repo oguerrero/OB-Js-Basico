91d5a76c1d7db243c975603eebb9454df19bac58
Oscar Guerrero <oscarguerrero242@gmail.com>
2022-09-02 14:20:41 -0500

    Ejercicio 15

---
+++ Ejercicio 15/index.js
@@ -0,0 +1,16 @@
+const nombre = 'Oscar'
+const apellido = 'Guerrero'
+
+const datos = {
+  nombre,
+  apellido
+}
+
+// Session Storage
+// sessionStorage.setItem('datos', JSON.stringify(datos))
+
+// Local Storage
+// localStorage.setItem('misDatos', JSON.stringify(datos))
+
+// Coockie
+// document.cookie = `datos = ${JSON.stringify(datos)}; max-age = 120`
